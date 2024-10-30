1bdab9755df5c5f62bda549d743bb8a6a2ae1798
yangss <1332551701@qq.com>
2024-10-30 17:50:26 +0800

    websocket 代理

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,10 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        // target: 'https://127.0.0.1:8811', // 你的后端服务器地址
         target: devServer.target,
+        pathRewrite: devServer.pathRewrite, // 重写路径：去掉路径中的/api部分
         changeOrigin: true,
-        pathRewrite: devServer.pathRewrite // 重写路径：去掉路径中的/api部分
+        ws: true
       }
     }
   }
