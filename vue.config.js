49498f2a3a824b110e7e49c08b045587f5c43986
yangss <1332551701@qq.com>
2024-11-01 10:40:56 +0800

    build 相关简单配置一下

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 const devServer = require('./devServer.json');
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '会员管理系统'; // 这里设置你的标题
+      return args;
+    })
+  },
   devServer: {
     proxy: {
       '/api': {
